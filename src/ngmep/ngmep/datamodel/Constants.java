c9774054a33f660fc9de66e76af5e52a9864b3ac
alberto <infjaf@gmail.com>
2012-12-05 01:20:13 +0100

    extract constants

---
+++ src/ngmep/ngmep/datamodel/Constants.java
@@ -0,0 +1,34 @@
+package ngmep.ngmep.datamodel;
+
+public class Constants {
+	public static final String KEY_NAME = "name";
+	public static final String KEY_NAME_ES = "name:es";
+	public static final String KEY_FIXME = "fixme";
+	public static final String KEY_BAD_INE_REF = "ine:ref";
+	public static final String KEY_SOURCE_ELE = "source:ele";
+	public static final String KEY_ELE = "ele";
+	public static final String KEY_POPULATION_DATE = "population:date";
+	public static final String KEY_SOURCE = "source";
+	public static final String KEY_OLD_NAME = "old_name";
+	public static final String KEY_OFFICIAL_NAME = "official_name";
+	public static final String KEY_LOC_NAME = "loc_name";
+	public static final String KEY_IS_IN_PROVINCE_CODE = "is_in:province_code";
+	public static final String KEY_CAPITAL = "capital";
+	public static final String KEY_ADMIN_LEVEL = "admin_level";
+	public static final String KEY_SOURCE_NAME = "source:name";
+	public static final String KEY_SOURCE_FILE = "source:file";
+	public static final String KEY_SOURCE_DATE = "source:date";
+	public static final String IGN = "Instituto Geográfico Nacional";
+	public static final String KEY_REF_INE = "ref:ine";
+	public static final String KEY_PLACE = "place";
+	public static final String KEY_POPULATION = "population";
+    public static final String ROLE_ADMIN_CENTRE = "admin_centre";
+    public static final String KEY_INE_MUNICIPIO = "ine:municipio";
+    public static final String ROLE_OUTER = "outer";
+
+	private Constants() {
+		super();
+		// No instances
+	}
+
+}
