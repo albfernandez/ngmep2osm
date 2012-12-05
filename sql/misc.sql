9d2f97df0871d843ef9042180b99cb1ee7589e3a
alberto <infjaf@gmail.com>
2012-12-05 12:11:47 +0100

    sql to reset deleted osmid

--- sql/misc.sql
+++ sql/misc.sql
@@ -6,4 +6,12 @@ osm_name = (select t.v from node_tags t where t.k = 'name' and t.node_id = n.osm
 autor = (select u.name from users u, nodes x where u.id = x.user_id and n.osmid = x.id)
 where 1=1
 and n.osmid is not null
-and exists (select 1 from poblaciones_osm p where p.id = n.osmid)
+and exists (select 1 from poblaciones_osm p where p.id = n.osmid);
+
+
+-- nulls deleted nodes or nodes with different ref-ine for reprocessing
+update ngmep n 
+set osmid = null
+where osmid is not null
+and not exists (select 1 from node_tags t where t.node_id = n.osmid and t.k = 'ref:ine' and t.v = n.cod_ine)
+and not exists (select 1 from way_tags  t where t.way_id  = n.osmid and t.k = 'ref:ine' and t.v = n.cod_ine);
